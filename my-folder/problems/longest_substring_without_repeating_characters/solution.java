86520db173a3ccc8e2bfda0fcc51b10078554c50
SahanaByrapu <101760308+SahanaByrapu@users.noreply.github.com>
2022-01-14 04:27:25 +0000

    Sync LeetCode submission - Longest Substring Without Repeating Characters (java)

--- my-folder/problems/longest_substring_without_repeating_characters/solution.java
+++ my-folder/problems/longest_substring_without_repeating_characters/solution.java
@@ -16,6 +16,7 @@ class Solution {
                left++;
            }
            
+           System.out.println(charfreq[r]);
           res=Math.max(res,right-left+1);
           right++;
        }
