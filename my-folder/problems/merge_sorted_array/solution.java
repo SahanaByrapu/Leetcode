ea2a3daaf9b6f254ae6918166fd9192f8cc2c688
SahanaByrapu <101760308+SahanaByrapu@users.noreply.github.com>
2022-06-05 17:30:21 +0000

    Sync LeetCode submission - Merge Sorted Array (java)

--- my-folder/problems/merge_sorted_array/solution.java
+++ my-folder/problems/merge_sorted_array/solution.java
@@ -1,27 +1,18 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-       
-        int l=0,r=0,p=0;
-        int [] nums1copy=new int[m];
         
-        for(int i=0;i<m;i++)
-            nums1copy[i]=nums1[i];
-            
-        while(p<m+n)
-        {
-            if(r>=n || (l<m && nums1copy[l]<nums2[r]))
-            {
-               nums1[p]=nums1copy[l];
-                l++;
-                p++;
-            }
-            else
-            {
-                nums1[p]=nums2[r];
-                r++;
-                p++;
-            }
-        }
+       int i,j,k; 
+        
+       for (i=m-1,j=n-1,k=m+n-1;k>=0;k--)
+       {
+           if(j<0)
+               break;
+           if(i>=0 && nums1[i] > nums2[j])
+              nums1[k]=nums1[i--];
+           else
+              nums1[k]=nums2[j--]; 
+       }
+        
         
         
     }
