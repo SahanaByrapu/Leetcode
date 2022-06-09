3f938a48498c1d97bb0ba52fffeea5fc44e46d00
SahanaByrapu <101760308+SahanaByrapu@users.noreply.github.com>
2022-06-09 17:51:41 +0000

    Sync LeetCode submission - Pow(x, n) (java)

---
+++ my-folder/problems/pow(x,_n)/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public double myPow(double x, int n) {
+      
+     
+       long N=n;
+       if(N<0)
+       {
+           x=1/x;
+           N=-N;
+       }
+        
+       double result=1;
+       double product=x;
+       for(long i=N;i>0;i/=2)
+       {
+          if(i%2 == 1) 
+              result*=product;
+           
+          product*=product; 
+       }
+        
+     return result;
+    }
+}
