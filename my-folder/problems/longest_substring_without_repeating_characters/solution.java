f446819fa256284efa65c062ba2e1cec9c5e2fe8
SahanaByrapu <101760308+SahanaByrapu@users.noreply.github.com>
2022-06-13 23:00:55 +0000

    Sync LeetCode submission - Longest Substring Without Repeating Characters (java)

--- my-folder/problems/longest_substring_without_repeating_characters/solution.java
+++ my-folder/problems/longest_substring_without_repeating_characters/solution.java
@@ -1,29 +1,19 @@
 class Solution {
-    
-     
     public int lengthOfLongestSubstring(String s) {
-     int n = s.length();
-     int []charfreq = new int[128];   
-     int res=0,left=0,right=0;
         
-       while(right < s.length())
+      int n=s.length() ; 
+       
+      HashMap<Character,Integer> map=new HashMap<Character,Integer>();
+      int maxlen=0; 
+      for(int right=0,left=0;right<n;right++)
       {
-           char r = s.charAt(right);
-           charfreq[r]++;
-           while(charfreq[r] > 1) {
-               char l=s.charAt(left);
-               charfreq[l]--;
-               left++;
-           }
-           
-           System.out.println(charfreq[r]);
-          res=Math.max(res,right-left+1);
-          right++;
-       }
-     
-        return res; 
-
-    }
+        if(map.get(s.charAt(right)) != null)
+            left= Math.max(map.get(s.charAt(right))+1,left);
+          map.put(s.charAt(right),right); 
+          maxlen=Math.max(maxlen,right-left+1);
+       }  
+          
+     return maxlen; 
+      }
     
-   
-}
+    }
