fed891e1a466c84a806ba2ca28643beb9836eff4
SahanaByrapu <101760308+SahanaByrapu@users.noreply.github.com>
2022-07-07 16:28:48 +0000

    Sync LeetCode submission - Copy List with Random Pointer (java)

---
+++ my-folder/problems/copy_list_with_random_pointer/solution.java
@@ -0,0 +1,54 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+    
+        if(head==null)
+            return null;
+        
+        Node curr=head;
+       
+        while(curr!=null)
+        {
+            Node newnode= new Node(curr.val);
+            newnode.next=curr.next;
+            curr.next=newnode;
+            curr=newnode.next;
+        }
+        
+        curr=head;
+         while(curr!=null)
+         {
+             curr.next.random=(curr.random!=null)? curr.random.next : null;
+             curr=curr.next.next;
+         }
+        
+        Node curr_old=head;
+        Node curr_new=head.next;
+        Node head_old=head.next;
+        
+        while(curr_old!=null)
+        {
+            curr_old.next=curr_old.next.next;
+            curr_new.next=(curr_new.next!=null)? curr_new.next.next:null;
+            curr_old=curr_old.next;
+            curr_new=curr_new.next;
+        }
+        
+        return head_old;
+        
+    }
+}
