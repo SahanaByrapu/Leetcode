6f7e2c19336e99f24700af817df8263d7520ef4e
SahanaByrapu <101760308+SahanaByrapu@users.noreply.github.com>
2022-08-05 16:52:10 +0000

    Sync LeetCode submission - Valid Parentheses (java)

--- my-folder/problems/valid_parentheses/solution.java
+++ my-folder/problems/valid_parentheses/solution.java
@@ -1,34 +1,31 @@
 class Solution {
     public boolean isValid(String s) {
-     
+        
+        Stack<Character> s1= new Stack<>();
         int n=s.length();
-        HashMap<Character,Character> mappings=new HashMap();
-        mappings.put(')','(');
-        mappings.put('}','{');
-        mappings.put(']','[');
         
-        int i,j,k;
-        
-        Stack<Character> stack = new Stack<Character>();
-        
-        for( i=0;i<n;i++)
+        for(int i=0;i<n;i++)
         {
-            char c=s.charAt(i) ;
-            if(mappings.containsKey(c))
+            char ch=s.charAt(i);
+            
+            if(ch == '(' || ch =='{' || ch=='[')
+                s1.add(ch);
+            
+            else
             {
+                char chr='#';
+                if(s1.isEmpty()) return false;
+                 chr=s1.pop();
                 
-                //stack.push(mappings.get(c));
-                char ch = stack.isEmpty() ? '#': stack.pop();
-                
-                if(ch != mappings.get(c))
-                   return false;
+                if ((ch==']'  &&  chr != '[') ||(ch =='}' &&  chr!='{') ||  
+                      (ch ==')' &&  chr!='(') ) 
+                 return false;
+                    
             }
-             else
-                   stack.push(c);
+            
         }
         
-        return stack.isEmpty();
+       return s1.isEmpty();
+            
     }
-    
-    
 }
