9094c09bfcb1892ee5b38d00492df0e6ace97ea2
SahanaByrapu <101760308+SahanaByrapu@users.noreply.github.com>
2023-02-12 17:41:45 +0000

    Sync LeetCode submission - Unique Paths (java)

--- my-folder/problems/unique_paths/solution.java
+++ my-folder/problems/unique_paths/solution.java
@@ -1,14 +1,15 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         
-    int N= m+n-2;
-    int r= m-1;
-    int i;
-    double res=1;
-            
-            for ( i = 1; i <= r; i++)
-                res = res * (N - r + i) / i;
-            return (int)res;
-       
+    int[][] grid=new int[m][n];
+    
+    for(int []arr:grid)
+     Arrays.fill(arr,1);
+    for(int i=1;i<m;i++)
+      for(int j=1;j<n;j++)
+         grid[i][j] =grid[i-1][j]+grid[i][j-1];
+
+      return grid[m-1][n-1];
+
     }
 }
