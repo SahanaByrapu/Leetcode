48c1c2e6ef8076b28cb38ac41ff003be24442472
SahanaByrapu <101760308+SahanaByrapu@users.noreply.github.com>
2023-02-20 01:14:12 +0000

    Sync LeetCode submission - Longest Substring Without Repeating Characters (java)

--- my-folder/problems/longest_substring_without_repeating_characters/solution.java
+++ my-folder/problems/longest_substring_without_repeating_characters/solution.java
@@ -1,19 +1,43 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         
-      int n=s.length() ; 
+       int n=s.length();   
+       int left=0;
+       int right=0;
+       int res=0;
+
+      HashMap<Character,Integer> hmap = new HashMap();
        
-      HashMap<Character,Integer> map=new HashMap<Character,Integer>();
-      int maxlen=0; 
-      for(int right=0,left=0;right<n;right++)
-      {
-        if(map.get(s.charAt(right)) != null)
-            left= Math.max(map.get(s.charAt(right))+1,left);
-          map.put(s.charAt(right),right); 
-          maxlen=Math.max(maxlen,right-left+1);
-       }  
-          
-     return maxlen; 
-      }
-    
+       //Sliding window
+       /*while(right<n)
+       {
+           char r= s.charAt(right);
+           hmap.put(r,hmap.getOrDefault(r,0)+1);
+
+           while(hmap.get(r)>1) 
+           {
+               char p=s.charAt(left);
+               hmap.put(p, hmap.get(p)-1);
+               left++;
+           }
+
+           res=Math.max(res,right-left+1);
+           right++;
+       }*/
+
+
+       //Sliding window-optimized
+       int res1=0;
+       for(int j=0,i=0;j<n;j++)
+       {
+           if(hmap.containsKey(s.charAt(j)))
+                i=Math.max(i,hmap.get(s.charAt(j)));
+            
+            res1=Math.max(res1,j-i+1);
+            hmap.put(s.charAt(j),j+1);
+
+       }
+
+        return res1;
     }
+}
