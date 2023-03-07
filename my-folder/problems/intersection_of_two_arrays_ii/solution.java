a678c384344001c51c8b5ab2d61afa1d90c1441d
SahanaByrapu <101760308+SahanaByrapu@users.noreply.github.com>
2023-03-07 03:10:53 +0000

    Sync LeetCode submission - Intersection of Two Arrays II (java)

--- my-folder/problems/intersection_of_two_arrays_ii/solution.java
+++ my-folder/problems/intersection_of_two_arrays_ii/solution.java
@@ -1,29 +1,38 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-       
-        int m= nums1.length;
-        int n= nums2.length;
         
-        int l=0,r=0,k=0;
         Arrays.sort(nums1);
         Arrays.sort(nums2);
-        while(l<m && r<n)
+
+        List<Integer> res=new ArrayList<Integer>();
+        int i=0,j=0;
+        while(i<nums1.length && j<nums2.length)
         {
-            if(nums1[l]==nums2[r]) 
-             { 
-                  nums1[k++] =nums1[l++];r++;
-             } 
-            else if(nums1[l]<nums2[r])
+          if(nums1[i] == nums2[j])
             {
-                l++;
+                res.add(nums1[i]);
+                i++;
+                j++;
             }
-            else
-            {
-                r++;
-            }
-          
+
+          else if(nums1[i]<nums2[j])
+              i++;
+          else if(nums1[i]>nums2[j])
+             j++;
+  
         }
         
-       return Arrays.copyOfRange(nums1,0,k);    
+        System.out.println(res.size());
+        
+        int ans[]= new int[res.size()];
+        int k=0;
+        for(int val :res)
+        {
+            ans[k]=val;
+            k++;
+        }
+
+        return  ans;
+        
     }
 }
