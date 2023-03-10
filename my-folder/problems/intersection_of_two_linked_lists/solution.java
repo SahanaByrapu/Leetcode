bb1fc4c0f3801a697d68321488ad26623f6b00ee
SahanaByrapu <101760308+SahanaByrapu@users.noreply.github.com>
2023-03-10 04:47:57 +0000

    Sync LeetCode submission - Intersection of Two Linked Lists (java)

---
+++ my-folder/problems/intersection_of_two_linked_lists/solution.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+       
+       if(headA==null || headB==null) return null;
+       
+       ListNode listA=headA;
+       ListNode listB=headB;
+
+       while(listA!=listB)
+       {
+           listA=(listA==null)?headB:listA.next;
+           listB=(listB==null)?headA:listB.next;
+
+       }
+
+        return listA;
+        
+    }
+}
