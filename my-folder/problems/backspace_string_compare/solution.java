0e7a274f7c9676ca2498224f6103447e143e424d
SahanaByrapu <101760308+SahanaByrapu@users.noreply.github.com>
2023-03-11 00:19:53 +0000

    Sync LeetCode submission - Backspace String Compare (java)

---
+++ my-folder/problems/backspace_string_compare/solution.java
@@ -0,0 +1,49 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+       
+    int skip1=0, skip2=0;
+    int i=s.length()-1, j=t.length()-1;
+
+    while(i>=0 || j>=0)
+    {
+      while(i>=0)
+      {
+         if(s.charAt(i)=='#')
+         {
+             skip1++;
+             i--;
+         }
+         else if(skip1>0)
+         {
+             skip1--;
+             i--;
+         }
+         else break;
+      }
+
+      while(j>=0)
+      { 
+          if(t.charAt(j)=='#')
+           {
+               skip2++;
+               j--;
+           }
+           else if(skip2>0)
+           {
+               skip2--;
+               j--;
+           }
+           else break;
+      }
+
+     if(i>=0 && j>=0 && s.charAt(i)!=t.charAt(j)) return false;
+     
+     if(i>=0^j>=0) return false;
+     
+     i--;j--;
+    }
+
+    return true;
+    }
+      
+}
