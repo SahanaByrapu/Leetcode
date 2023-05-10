7757c4576afb1e9ce345257b5a94bf4078ded810
SahanaByrapu <101760308+SahanaByrapu@users.noreply.github.com>
2023-05-10 17:25:35 -0400

    Time: 62 ms, Memory: 52.1 MB - LeetHub

--- fruit-into-baskets/fruit-into-baskets.java
+++ fruit-into-baskets/fruit-into-baskets.java
@@ -1,10 +1,10 @@
 class Solution {
     public int totalFruit(int[] fruits) {
         
-        int sliding_window=0;
+        
         HashMap<Integer,Integer> basket=new HashMap();
         int i,j;
-        for(i=0,j=0;i<fruits.length;i++)
+        /*for(i=0,j=0;i<fruits.length;i++)
         {
            basket.put(fruits[i],basket.getOrDefault(fruits[i],0)+1);
 
@@ -16,8 +16,29 @@ class Solution {
               j++;
             }
 
+        }*/
+
+       
+        
+        //sliding window - optimized
+        int maxcount=0;
+        for(i=0,j=0;i<fruits.length;i++)
+        {
+           basket.put(fruits[i],basket.getOrDefault(fruits[i],0)+1);
+
+           while(basket.size()>2) 
+            {
+               basket.put(fruits[j],basket.get(fruits[j])-1);
+              if(basket.get(fruits[j])==0)
+                basket.remove(fruits[j]);
+              j++;
+            }
+
+          maxcount=Math.max(maxcount,i-j+1);
         }
 
-        return i-j;
+        return maxcount;
+
+        //return i-j;
     }
 }
